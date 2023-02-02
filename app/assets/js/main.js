f709da7b536d24271f6942f538991667eae5ca12
Thiago <thiagocordeirodantas0@Gmail.com>
2023-02-02 17:30:42 -0300

    sobre mim finalizado

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -24,8 +24,16 @@ function email (){
     document.querySelector('.botao-email').addEventListener('click',function Emaiel(){
         return  window.location.href = 'https://mail.google.com/mail/u/0/#sent?compose=CllgCJqSvnbrGWVLDnnMtbHnCkSJhKjzZhtkrNtKRHmmvndklvtwScZtqRQvQmJdFwshpdxGCqB'
 })}
+
+
+function curriculo(){
+    document.querySelector('.botao-curriculo').addEventListener('click', function BaixarCurriculo() {
+       return window.location.href = 'https://doc-00-18-docs.googleusercontent.com/docs/securesc/ndv8enqq2rhgf1h8l64nbm5g64btiml3/h840klk3lh77dv3kp7esp9bd9uak1ima/1675369725000/00795112751166814111/00795112751166814111/1yU7XIqPHZF09dPc4mNtG_aS8GIZ4Gwmu?e=download&ax=AB85Z1B2EbDWz0tOrRTgMg-HuT7H7q3Dx8vXLyuOFqiGS94O3p4RSqyB58fCY2olonS3-7mZGcbPoXZpcc4OEOGRvn6WlBw5kBpTA68ZouozOrnaVCLh7X-sVgZdtsPWguu3-ypB6zuCbBAh6yic1kWLH1v8VLSAWmVG9kVYV7uZsmUoVcQYv0j55h-agTwFy7n9heHI8Mfb98AZROxXmT84QT8WEfhbcBdyTTQD1bV6KQXKaTOkMlKvSFasYtr1l2IidqGQR6udComtYkhS46FfPtcvz-TXvdl-MVXmaUWngDcXzHRpyx8Sua8JSo0pGmYYvMDIHJhZDOi-JqGQv5vNNRD9AWBgGwBQiDTS2ilbXpw67ugFVtUqiCWH4fC2x0_kicqKxxLiEuZ6ZCieD-x2uR_R2NNodQ978mIAXvWu_G92qKtN6rQl-KZ_sHRMVCQNneklwhgcJ1gl1sLN-1b6UDLYtYkbEkJ2GhezIFwAqEbne5u4fRu6Oh64ZaMbGvtbrYubbVD6R6mvl0CQUnpzJna08qmwKRv9jIsfhczmdUDNaILznKENc2jwvCNdoftsZKThK2hb2c6Xj-UosnZtRzhYqnX_rmp0YNX7kbTofq5PuQMNV6o6u41MxbOyaEfISftC0BtQwg3BSGPYDv2sKHq09VKqPpFZAbWQBuuF4-e-f3w6L8fgzfJ2IyNO8E3F_nzsBk_dwajDgU7_1lZlDvivKUvnhNPlHHPKDNH1-jo13ALmyxLbpUNXXMAFTsdciFR9y4FPHsqqW-i7bkCfch_GYRw3Qz9xevbp0-YxwClVGGYNm86N5YH5-_tznBaeE59FFT_5MxPXkyf5IRN5QWagEN8pOZa28FAMKVxVYmt11AVTmJRhIcA2jb6hZ1S-sxQrdKiSi9cOUFBRWv58FBpMZxssm3UWhR0&uuid=ecb2967b-39b0-4b82-bd3d-fa28cbd2aa31&authuser=0&nonce=3t7njak1d06jk&user=00795112751166814111&hash=c7p0081pos6a8i43jatcpucn1m6jd2nj'
+    }
+)}
    
        
 botaoLinkedin();
 botaoGitHub();
 email();
+curriculo();
