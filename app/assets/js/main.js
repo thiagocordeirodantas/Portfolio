32be8749fb358cfe3f3e5c9908b909645952e3f2
Thiago <thiagocordeirodantas0@Gmail.com>
2023-02-02 15:31:15 -0300

    manipulando so botoes

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,1 +1,22 @@
 ScrollReveal().reveal('.headline');
+
+function botaoLinkedin() {
+    document.querySelector('.botao-1').addEventListener('click',function botaoGIT(){
+        return  window.location.href = 'https://www.linkedin.com/in/thiagocordeirodantas/'
+    })
+}
+
+
+
+ function botaoGitHub() {
+    document.querySelector('.botao-2').addEventListener('click',function botaoGIT(){
+        return  window.location.href = 'https://github.com/Melieny'
+    })
+}
+
+
+
+   
+       
+botaoLinkedin();
+botaoGitHub();
